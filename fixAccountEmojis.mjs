24e4e3373565fe5759db0fba9492c7e57488ba2a
Will Smidlein <will@willsmidlein.com>
2022-08-14 16:14:16 -0600

    Fix formatting

--- fixAccountEmojis.mjs
+++ fixAccountEmojis.mjs
@@ -18,7 +18,7 @@ const l = chalk.blue("|");
 const { data } = await ynab.accounts.getAccounts(BUDGET_ID);
 const { accounts } = data;
 
-console.log('YNAB currently does not have an API to update account name- instead you have to manually make the following updates:')
+console.log("YNAB currently does not have an API to update account name- instead you have to manually make the following updates:");
 
 for (const account of accounts) {
   const emojis = onlyEmoji(account.name).join("");
@@ -27,19 +27,19 @@ for (const account of accounts) {
   if (emojis.length >= 1) {
     const name = withoutEmoji(account.name).join("").trim();
 
-    const newName = `${emojis} ${name}`; 
+    const newName = `${emojis} ${name}`;
 
     if (account.name != newName) {
-        console.log(`${l}${account.name}${l} to ${l}${newName}${l}`)
-    //   const shouldContinue = await confirm(`Confirm you'd like to rename ${l}${account.name}${l} to ${l}${newName}${l}?`);
-    //   if (shouldContinue) {
-    //     // omg there's no account update API
-    //     console.log("Success");
-    //   } else {
-    //     console.log("Skipping...");
-    //   }
+      console.log(`${l}${account.name}${l} to ${l}${newName}${l}`);
+      //   const shouldContinue = await confirm(`Confirm you'd like to rename ${l}${account.name}${l} to ${l}${newName}${l}?`);
+      //   if (shouldContinue) {
+      //     // omg there's no account update API
+      //     console.log("Success");
+      //   } else {
+      //     console.log("Skipping...");
+      //   }
     }
   }
 }
 
-console.log("Done")
+console.log("Done");
