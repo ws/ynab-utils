9d2da481c130da2a722f82849166754061111bc1
Will Smidlein <will@willsmidlein.com>
2022-08-13 19:18:32 -0600

    Utilception

---
+++ util.mjs
@@ -0,0 +1,14 @@
+export const renderUSD = (milliunits) => {
+	const units = milliunits / 1000;
+
+	const formatter = new Intl.NumberFormat("en-US", {
+		style: "currency",
+		currency: "USD"
+	});
+
+	if (milliunits < 0) {
+		return chalk.red(`-${formatter.format(-units)}`);
+	}
+	return chalk.green(`+${formatter.format(units)}`);
+};
+
